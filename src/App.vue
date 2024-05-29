b9cbb6b383c934640b68418f41773fa075144950
nebociel <akashchaubey443@gmail.com>
2024-05-29 22:03:27 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <p>{{ sourceData }}</p>
+  </div>
+</template>
+
+<script setup>
+import sourceData from '@/data.json';
+
+</script>
+
+<style scoped></style>
